28fdb135a38449493058a39bcde9c3898420192b
viridius-hub <smiledie-hub@yandex.ru>
2023-12-17 20:28:31 +0500

    init

--- bot_telegram/src/messageHandlers/messageHandlers.ts
+++ bot_telegram/src/messageHandlers/messageHandlers.ts
@@ -1,40 +1,65 @@
 import {postToApi, getFromApi} from "../api/apiService";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
+import {Context} from "node:vm";
 
-export const handleTextMessage = async (ctx: any) => {
+interface TelegramErrorResponse {
+    ok: boolean;
+    error_code: number;
+    description: string;
+    parameters?: {
+        retry_after: number;
+    };
+}
+
+export const handleTextMessage = async (ctx: Context) => {
     try {
         const postResponse = await postToApi(ctx.message.text);
 
         if (postResponse) {
-            const requestId = postResponse.data.id
+            const requestId = postResponse.data.id;
 
             const checkResponse = async () => {
                 try {
-                    const getResponse = await axios.get(`${process.env.API_LINK}/${requestId}`)
+                    const getResponse = await getFromApi(requestId);
                     if (getResponse.data.status === 'success') {
-                        clearInterval(checkInterval)
+                        clearInterval(checkInterval);
                         const replyMessage = getResponse.data.response.text;
-                        ctx.reply(replyMessage)
-                        ctx.telegram.sendChatAction(ctx.chat.id, 'typing')
+                        ctx.reply(replyMessage);
+                        await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
                     } else {
-                        ctx.telegram.sendChatAction(ctx.chat.id, 'typing')
+                        await ctx.telegram.sendChatAction(ctx.chat.id, 'typing');
                     }
                 } catch (error) {
-                    clearInterval(checkInterval)
-                    console.error(error)
-                    ctx.reply(`Произошла ошибка при получении ответа: ${error}`)
+                    if (axios.isAxiosError(error)) {
+                        const axiosError = error as AxiosError<TelegramErrorResponse>;
+                        if (axiosError.response && axiosError.response.status === 429) {
+                            const retryAfter = (axiosError.response.data.parameters?.retry_after ?? 1) * 1000;
+                            console.log(`Waiting for ${retryAfter / 1000} seconds`);
+                            await new Promise(resolve => setTimeout(resolve, retryAfter));
+                            checkResponse();
+                        } else {
+                            clearInterval(checkInterval);
+                            console.error(error);
+                            ctx.reply(`Произошла ошибка при получении ответа: ${error}`);
+                        }
+                    } else {
+                        clearInterval(checkInterval);
+                        console.error(error);
+                        ctx.reply(`Произошла ошибка: ${error}`);
+                    }
                 }
-            }
-            const checkInterval = setInterval(checkResponse, 500)
+            };
+            const checkInterval = setInterval(checkResponse, 500);
         }
 
     } catch (error) {
-        console.error(error)
-        ctx.reply(`Произошла ошибка: ${error}`)
+        console.error(error);
+        ctx.reply(`Произошла ошибка: ${error}`);
     }
-}
+};
 
-export const handleStickerMessage = async (ctx: any) => {
+
+export const handleStickerMessage = async (ctx: Context) => {
     const stickerFileId = ctx.message.sticker.file_id
     ctx.replyWithSticker(stickerFileId)
 }
